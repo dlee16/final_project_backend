2b27e555fd2a4cc2808af268f36a85576bb935f4
dlee16 <diana.lee116@gmail.com>
2019-05-14 17:06:42 -0400

    updates to controllers

--- app/controllers/lifestages_controller.rb
+++ app/controllers/lifestages_controller.rb
@@ -1,2 +1,12 @@
 class LifestagesController < ApplicationController
+    def index 
+        @lifestage = Lifestage.all 
+        render json: @lifestage
+    end 
+
+    def show
+        @lifestage = Lifstage.find(params[:id])
+        render json: @lifestage
+    end 
+
 end
--- app/controllers/user_lifestages_controller.rb
+++ app/controllers/user_lifestages_controller.rb
@@ -1,2 +1,13 @@
 class UserLifestagesController < ApplicationController
+
+    def index 
+        @userls = User_lifestages.all 
+        render json: @userls
+    end 
+
+    def show
+        @userls = User_lifestage.find(params[:id])
+        render json: @userls
+    end 
+
 end
