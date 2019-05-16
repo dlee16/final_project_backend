ab78c0208613582a132c7a051eb6c240934d44c9
dlee16 <diana.lee116@gmail.com>
2019-05-16 11:06:56 -0400

    updated group serializer

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -1,5 +1,10 @@
 class GroupSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :lifestage_id
-  belongs_to :lifestage
+  attributes :id, :name, :description, :lifestage_id, :lifestage_name
+  # belongs_to :lifestage
   has_many :users
+  has_many :comments
 end
+
+def lifestage_name
+  self.lifestage.name
+end 
