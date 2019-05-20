3cd7fbf937fae67d85f5d178151b83a92731ac72
dlee16 <diana.lee116@gmail.com>
2019-05-20 09:57:01 -0400

    updated serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,5 +1,9 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :group_id, :user_comment
+  attributes :id, :user_id, :group_id, :user_comment, :user_name
   belongs_to :user
   belongs_to :group
+end
+
+def user_name
+  self.user.name
 end
